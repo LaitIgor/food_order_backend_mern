912f50bb49f4d4aa09de19a99cc40e6250a8e4b6
Lait Igor <laitigorofficial@gmail.com>
2024-03-02 18:50:36 +0100

    initial set up of backend server, create index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 
 const app = express();
+
+// this call allows app to take JSON requests as body
+app.use(express.json());
+app.use(cors());
+
+app.get('/test', async (req: Request, res: Response,) => {
+  res.json({ message: 'Response from backend' })
+})
+
+app.listen('8800', () => {
+  console.log('Server launched on port 8800 successfully!');
+})
