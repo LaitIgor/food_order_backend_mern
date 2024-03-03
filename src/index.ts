bb527b6944500a6ee2112049aeec77a3918e219e
Lait Igor <laitigorofficial@gmail.com>
2024-03-03 19:10:33 +0100

    add connection to mongo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_DB_URI as string)
+  .then(() => console.log('Connected to DB'));
 
 const app = express();
 
