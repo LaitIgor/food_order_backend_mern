134bd13752f5da6293a58a067bdbaac9fca6de9d
Lait Igor <laitigorofficial@gmail.com>
2024-03-08 20:21:14 +0100

    saving new user to DB

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
 const createCurrentUser = async (req: Request, res: Response) => {
   // Check if user exists
   // Create a new user if does not exist
   // return user object
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+
+    if (existingUser) {
+      return res.status(200).send();
+    }
+    const newUser = new User(req.body);
+    await newUser.save();
+    return res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log('error',error );
+    res.status(500).json({ message: 'There was an error while creating user' })
+  }
 
 }
 
