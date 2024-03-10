aa5e84f6fce9e4d4cdc2da33a14cc5d82fdd306e
Lait Igor <laitigorofficial@gmail.com>
2024-03-10 18:39:54 +0100

    add auth0 token check, token parse, req.body validation for user creation and update

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import MyUserController from '../controllers/MyUserController';
+import { jwtCheck, jwtPars } from '../middleware/auth';
+import { validateMyUserRequest } from '../middleware/validation';
 
 const router = express.Router();
 
-router.post('/', MyUserController.createCurrentUser);
+router.post('/', jwtCheck, MyUserController.createCurrentUser);
+router.put('/', jwtCheck, jwtPars, validateMyUserRequest, MyUserController.updateCurrentUser)
 
 export default router;
